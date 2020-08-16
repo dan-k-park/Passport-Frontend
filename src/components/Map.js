03567d432c737d79e36f215cc3186febefb9f16f
Daniel Park <parkdk96@gmail.com>
2020-08-15 21:13:58 -1000

    more layout stuff

--- src/components/Map.js
+++ src/components/Map.js
@@ -56,56 +56,57 @@ class Map extends Component {
 
   render() {
     return (
-      <Grid columns={3} divided>
+      <Grid stackable divided columns={2} padded={true}>
         <Grid.Row stretched>
-          <Grid.Column>
-            <Segment>My Trips</Segment>
-            <Segment>Top Trips</Segment>
-          </Grid.Column>
-          <Grid.Column>
+        <Grid.Column width={11}>
             <Segment>
-            <VectorMap
-        map={"world_mill"}
-        backgroundColor={"transparent"}//change it to ocean blue: #0077be
-        zoomOnScroll={true}
-        panOnDrag={true}
-        containerStyle={{
-          width: "100%",
-          height: "520px"
-        }}
-        onRegionClick={this.handleClick} //gets the country code
-        containerClassName="map"
-        regionStyle={{
-          initial: {
-            fill: "#e4e4e4",
-            "fill-opacity": 0.9,
-            stroke: "none",
-            "stroke-width": 0,
-            "stroke-opacity": 0
-          },
-          hover: {
-            "fill-opacity": 0.8,
-            cursor: "pointer"
-          },
-          selected: {
-            fill: "#2938bc" //color for the clicked country
-          },
-          selectedHover: {}
-        }}
-        regionsSelectable={true}
-        series={{
-          regions: [
-            {
-              values: this.state.data, //this is your data
-              scale: ["#146804", "#ff0000"], //your color game's here
-              normalizeFunction: "polynomial"
-            }
-          ]
-        }}
-      />
+              <VectorMap
+                map={"world_mill"}
+                backgroundColor={"transparent"}//change it to ocean blue: #0077be
+                zoomOnScroll={true}
+                panOnDrag={true}
+                containerStyle={{
+                  width: "100%",
+                  height: "520px"
+                }}
+                onRegionClick={this.handleClick} //gets the country code
+                containerClassName="map"
+                regionStyle={{
+                  initial: {
+                    fill: "#e4e4e4",
+                    "fill-opacity": 0.9,
+                    stroke: "none",
+                    "stroke-width": 0,
+                    "stroke-opacity": 0
+                  },
+                  hover: {
+                    "fill-opacity": 0.8,
+                    cursor: "pointer"
+                  },
+                  selected: {
+                    fill: "#2938bc" //color for the clicked country
+                  },
+                  selectedHover: {}
+                }}
+                regionsSelectable={true}
+                series={{
+                  regions: [
+                    {
+                      values: this.state.data, //this is your data
+                      scale: ["#146804", "#ff0000"], //your color game's here
+                      normalizeFunction: "polynomial"
+                    }
+                  ]
+                }}
+              />
             </Segment>
           </Grid.Column>
+        <Grid.Column width={5}>
+          <Segment>My Trips</Segment>
+          <Segment>Top Trips</Segment>
+        </Grid.Column>
         </Grid.Row>
+        
       </Grid>
     )
   }
